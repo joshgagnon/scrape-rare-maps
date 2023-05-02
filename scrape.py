960f51e94db1b7871e60ce8beadbcbbc812426ac
Josh Gagnon <josh.n.gagnon@gmail.com>
2023-05-02 11:37:54 +1000

    woot, it's fast now

--- scrape.py
+++ scrape.py
@@ -1,14 +1,16 @@
 import requests
-import json
-import re
 import sys
 from lxml import etree
 import math
 import aiohttp
 import asyncio
 from collections import namedtuple
+from PIL import Image, ImageChops
+import io
+
 
 Tile = namedtuple("Tile", "x y img")
+Image.MAX_IMAGE_PIXELS = 933120000
 
 tile_url = "https://storage.googleapis.com/raremaps/img/dzi/img_%s_files/%d/%d_%d.jpg"
 metadata_url = "https://storage.googleapis.com/raremaps/img/dzi/img_%s.dzi"
@@ -37,22 +39,38 @@ async def get_tile(session, x, y, url):
     async with session.get(url) as resp:
         result = await resp.content.read()
         print('Got %s' % url)
-        return Tile(x, y, result)
+        return Tile(x, y, io.BytesIO(result))
 
 
 async def get_tiles(attributes):
-    connector = aiohttp.TCPConnector(limit=25)
+    connector = aiohttp.TCPConnector(limit=100)
     async with aiohttp.ClientSession(connector=connector, headers=headers) as session:
 
         tasks = []
-        for x in range(0, 5): #attributes['X']):
-            for y in range(0, 5): # attributes['Y']):
+        for x in range(0, attributes['X']):
+            for y in range(0, attributes['Y']):
                 url = tile_url % (attributes['Code'], attributes['Zoom'], x, y)
                 tasks.append(asyncio.ensure_future(get_tile(session, x, y, url)))
 
         tiles = await asyncio.gather(*tasks)
         print('Got em')
-        print(tiles)
+        return tiles
+
+def stitch(attributes, tiles):
+    print('Stitching')
+    img = Image.new('RGB', (attributes['Width'], attributes['Height']))
+    for tile in tiles:
+        tile_img = Image.open(tile.img)
+        img.paste(im=tile_img, box=((tile.x * attributes['TileSize']) or 1, (tile.y * attributes['TileSize']) or 1))
+
+    bg = Image.new(img.mode, img.size, img.getpixel((img.size[0]-1,img.size[1]-1)))
+    diff = ImageChops.difference(img, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)  
+    bbox = diff.getbbox()
+    if bbox:
+        return img.crop(bbox)  
+    return img
+        
 
 async def scrape(code):
     attributes = parse_xml(get_xml(code))
@@ -60,19 +78,9 @@ async def scrape(code):
     attributes['X'] = int(attributes['Width'] / (attributes['TileSize'] - attributes['Overlap'])) + 1
     attributes['Y'] = int(attributes['Height'] / (attributes['TileSize'] - attributes['Overlap'])) + 1
     attributes['Code'] = code
-    await get_tiles(attributes)
+    tiles = await get_tiles(attributes)
+    result = stitch(attributes, tiles)
+    result.save('./output.jpg')
 
 if __name__ == "__main__":
     asyncio.run(scrape(sys.argv[1]))
-
-if None:
-
-    page = requests.get(this_url, headers=headers)
-
-    for x in range(0, x_size+1):
-        for y in range(0, y_size+1):
-            this_url = url % (x,y)
-            print(this_url)
-            page = requests.get(this_url, headers=headers)
-            with open('./tile/%d_%d.jpg' % (zoom, x, y), "wb") as f:
-                f.write(page.content)
